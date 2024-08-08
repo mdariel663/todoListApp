61f17e7d1b0073f64f436999ab7552bedcae997c
Mario Dariel <mdariel663@gmail.com>
2024-08-08 07:58:10 -0400

    Creando Modelos To-Do

---
+++ app/src/main/java/com/i_mario/agrodirect/ToDoModel.java
@@ -0,0 +1,31 @@
+package com.i_mario.agrodirect;
+
+public class Todo {
+    private long id;
+    private String text;
+    private boolean completed;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
